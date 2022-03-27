056380f6fd58ad38f8771ecb3420ccc6380bf6a2
2562302784 <101639438+2562302784@users.noreply.github.com>
2022-03-27 22:38:32 +0800

    Add files via upload

--- 清.c
+++ 清.c
@@ -1,74 +1,46 @@
-#include<stdio.h>
-#include<windows.h>
-#include<conio.h>
-#define ROW 9
-#define COL 11
-char map[ROW][COL+1]={
-        {"*#*********"},
-		{"***###*###*"},
-		{"###**#****#"},
-		{"*#**###**#*"},
-		{"***********"},
-        {"#####*##*##"},
-		{"**#*****#*E"},
-		{"***#*###**#"},
-		{"*#*********"},
-};
-
-void print_map()
+#include<stdio.h>                                                             
+#include<string.h>
+int n,i,pro,nol,m,s,j,c;
+int pro_num[1000];
+char str[1000][20];
+int nol_num[1000],a[1000];
+int main(void)
 {
-	for(int i=0;i<ROW;i++)
-	{
-		puts(map[i]);
-	}
+	printf("请输入要录入考生信息的人数：");
+        scanf("%d",&n);
+        for(i=0;i<n;i++)
+		{
+			printf("输入考生准考证号、试机座位号、考生座位号分别用空格隔开：");
+			scanf("%s %d %d",str[i],&pro,&nol);
+            pro_num[i]=pro;
+            nol_num[i]=nol;
+		}
+		printf("请输入需要查询的人数：");
+        scanf("%d",&m);
+        for(i=0;i<m;i++)
+		{
+			printf("输入试机座位号:");
+			scanf("%d",&pro);
+			a[i]=pro;
+        }
+        j=0;
+        while(j<m)
+		{
+                for(i=0;i<n;i++)
+				{
+                        if(a[j]==pro_num[i])
+						{		
+							printf("准考证号为：");
+							for(c=0;c<14;c++)
+							{
+								printf("%c",str[i][c]);
+							}
+                                printf(" 考试座位号为：%d\n",nol_num[i]);
+                                j++;
+                               break;
+                        }
+                }
+ 
+        }
+        return 0;
 }
-
-void show_cursor(int x,int y)
-{
-	COORD pos;
-	pos.X=x;
-	pos.Y=y;
-	printf("curX=%d,curY=%d\n",x,y);
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
-}
-
-int curX,curY;
-int main()
-{
-	printf("请按迷宫走出去，向上为w向下为s左边a右边d");
-	while(1)
-	{
-		system("cls");
-		print_map();
-		show_cursor(curX,curY);
-		char t=getch();
-		if(t=='w')
-		{
-			if((curY-1)>=0&&(map[curY-1][curX]=='*'||map[curY-1][curX]=='E')) curY--;
-		}
-		else if(t=='s')
-		{
-			if((curY+1)<ROW&&(map[curY+1][curX]=='*'||map[curY+1][curX]=='E')) curY++;
-		}
-		else if(t=='s')
-		{
-			if((curY+1)<ROW&&(map[curY+1][curX]=='*'||map[curY+1][curX]=='E')) curY++;
-			
-		}
-		else if(t=='a')
-		{
-			if((curX-1)>=0&&(map[curY][curX-1]=='*'||map[curY][curX-1]=='E')) curX--;
-		}
-		else if(t='d')
-		{
-			if((curX+1)<COL&&(map[curY][curX+1]=='*'||map[curY][curX+1]=='E'))curX++;
-		}
-		if(map[curY][curX]=='E')
-		{   
-		    printf("恭喜你呀，获得重生了\t");
-			break;
-		}
-	}
-	return 0;
-}
-
